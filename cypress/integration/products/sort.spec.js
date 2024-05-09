c52c52de4e668a5f99bc3d3b31351c845aba6120
rafaelponzio <rafael.ponzio@dasa.com.br>
2024-05-09 09:57:04 -0300

    Sort test case

---
+++ cypress/integration/products/sort.spec.js
@@ -0,0 +1,46 @@
+// <reference types="cypress"/>
+
+const users = require('../../fixtures/users.json')
+
+const { user, password } = users['standard']
+
+describe('Sort', () => {
+    before(() => {      
+
+      cy.visit('/')
+      cy.login(user, password)
+      
+      cy.url()
+        .should('include', '/inventory.html')
+    })
+  
+    it('should sort products for Name(A to Z)', () => {            
+
+      cy.getByDataTest('product-sort-container').select('az')
+      cy.get('.inventory_item_name').first().should('have.text', 'Sauce Labs Backpack')
+
+    })
+
+    it('should sort products for Name (Z to A)', () => {            
+
+      cy.getByDataTest('product-sort-container').select('za')
+      cy.get('.inventory_item_name').first().should('have.text', 'Test.allTheThings() T-Shirt (Red)')
+      
+    })
+
+    it('should sort products for Price (low to high)', () => {            
+
+      cy.getByDataTest('product-sort-container').select('lohi')
+      cy.get('.inventory_item_price').first().should('have.text', '$7.99')
+      
+    })
+
+    it('should sort products for Price (high to low)', () => {            
+
+      cy.getByDataTest('product-sort-container').select('hilo')
+      cy.get('.inventory_item_price').first().should('have.text', '$49.99')
+      
+    })
+  })
+
+  
